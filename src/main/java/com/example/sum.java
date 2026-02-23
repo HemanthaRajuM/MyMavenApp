e38c430a631d4ccbb4cb840b77b8db0de4dd3723
HemanthaRajuM <hemantharaju04@gmail.com>
2026-02-23 09:45:48 +0530

    second commit

---
+++ src/main/java/com/example/sum.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class sum {
+    public static void main(String[] args) {  // Fixed: string → String
+        int a = 5;
+        int b = 8;
+        System.out.println("Sum: " + (a + b));
+    }
+}
+
